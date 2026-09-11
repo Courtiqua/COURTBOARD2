aa5952be7c990b561517d1f3941b9ad5da4f8e21
Courtiqua <turner_courtney1@icloud.com>
2026-09-11 10:19:08 +0100

    Add files via upload
    fixed courtboard app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "courtboard-v3";
+const CACHE = "courtboard-v3-1";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
